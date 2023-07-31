31863db5b5b07cbbb6854a103e5bf0ddc2582ea8
tanvir243 <travelertanvir@gmail.com>
2023-07-31 21:52:25 +0600

    18. Courses Parchasing Simple React Bootstrap Project made of mine

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import logo from '../../image/logo.png';
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
 import './Header.css'
 
 const Header = () => {
